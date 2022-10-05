7b53614bae900416242838671aea5df6bcf54f20
Modeesha-Kalani <modeeshakalani97@gmail.com>
2022-10-06 03:01:24 +0530

    Student API modified

--- backend/routes/api/students.js
+++ backend/routes/api/students.js
@@ -51,4 +51,77 @@ router.post('/register', (req, res) => {
         });
         }
     });
+    });
+
+// @route   POST api/student/login
+// @desc    Login student / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+    
+    //Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
     }
+    
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    //Find user by email
+    student.findOne({ email }).then(user => {
+        //Check for user
+        if (!user) {
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
+        }
+    
+        //Check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+            //User matched
+    
+            const payload = { id: user.id, name: user.name }; //Create JWT Payload
+    
+            //Sign Token
+            jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+                res.json({
+                success: true,
+                token: 'Bearer ' + token
+                });
+            }
+            );
+        } else {
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors);
+        }
+        });
+    });
+    });
+
+// @route   GET api/student/current
+// @desc    Return current student
+// @access  Private
+router.get(
+    '/current',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+        });
+    }
+    );
+
+//Get all students
+router.get('/', (req, res) => {
+    student.find()
+    .then(students => res.json(students))
+    .catch(err => res.status(404).json({ nostudentsfound: 'No students found' }));
+});
+
+module.exports = router;
